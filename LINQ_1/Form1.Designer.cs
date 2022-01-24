9373bdcb6634cdaff9d63c79e95386bca7268e9d
Willian Ribeiro <willianbuonaduce@gmail.com>
2022-01-24 20:31:41 -0300

    LINQ

---
+++ LINQ_1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace LINQ_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnWhere = new System.Windows.Forms.Button();
+            this.btnOrderBy = new System.Windows.Forms.Button();
+            this.btnGroupBy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 31;
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(542, 407);
+            this.lista.TabIndex = 0;
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConsulta.Location = new System.Drawing.Point(562, 13);
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(226, 35);
+            this.txtConsulta.TabIndex = 1;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(562, 55);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(226, 40);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnWhere
+            // 
+            this.btnWhere.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWhere.Location = new System.Drawing.Point(561, 101);
+            this.btnWhere.Name = "btnWhere";
+            this.btnWhere.Size = new System.Drawing.Size(226, 40);
+            this.btnWhere.TabIndex = 3;
+            this.btnWhere.Text = "where";
+            this.btnWhere.UseVisualStyleBackColor = true;
+            this.btnWhere.Click += new System.EventHandler(this.btnWhere_Click);
+            // 
+            // btnOrderBy
+            // 
+            this.btnOrderBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrderBy.Location = new System.Drawing.Point(561, 147);
+            this.btnOrderBy.Name = "btnOrderBy";
+            this.btnOrderBy.Size = new System.Drawing.Size(226, 40);
+            this.btnOrderBy.TabIndex = 4;
+            this.btnOrderBy.Text = "order by";
+            this.btnOrderBy.UseVisualStyleBackColor = true;
+            this.btnOrderBy.Click += new System.EventHandler(this.btnOrderBy_Click);
+            // 
+            // btnGroupBy
+            // 
+            this.btnGroupBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGroupBy.Location = new System.Drawing.Point(562, 193);
+            this.btnGroupBy.Name = "btnGroupBy";
+            this.btnGroupBy.Size = new System.Drawing.Size(226, 40);
+            this.btnGroupBy.TabIndex = 5;
+            this.btnGroupBy.Text = "group by";
+            this.btnGroupBy.UseVisualStyleBackColor = true;
+            this.btnGroupBy.Click += new System.EventHandler(this.btnGroupBy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGroupBy);
+            this.Controls.Add(this.btnOrderBy);
+            this.Controls.Add(this.btnWhere);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnWhere;
+        private System.Windows.Forms.Button btnOrderBy;
+        private System.Windows.Forms.Button btnGroupBy;
+    }
+}
+
---
+++ LINQ_1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ_1
+{
+    public partial class Form1 : Form
+    {
+        List<string> lista_nomes;
+        List<int> lista_numeros;
+        Dictionary<string, double> lista_produtos;
+        Dictionary<string, string> lista_estados;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            #region Lista de Nomes
+            lista_nomes = new List<string>();
+            lista_nomes.Add("Willian");
+            lista_nomes.Add("Danny");
+            lista_nomes.Add("Gabriel");
+            lista_nomes.Add("Thiago");
+            lista_nomes.Add("Gabriela");
+            lista_nomes.Add("Rafael");
+            lista_nomes.Add("Rafaela");
+            lista_nomes.Add("Maria");
+            lista_nomes.Add("Joao");
+            lista_nomes.Add("Manuela");
+            lista_nomes.Add("Joao");
+            #endregion
+
+            #region Lista Números
+            lista_numeros = new List<int>();
+            lista_numeros.Add(10);
+            lista_numeros.Add(5);
+            lista_numeros.Add(2);
+            lista_numeros.Add(20);
+            lista_numeros.Add(13);
+            lista_numeros.Add(9);
+            lista_numeros.Add(18);
+            #endregion Números
+
+            #region Lista Produtos
+            lista_produtos = new Dictionary<string, double>();
+            lista_produtos.Add("Teclado USB", 49.95);
+            lista_produtos.Add("Teclado PS2", 45.95);
+            lista_produtos.Add("Teclado Gamer", 358.99);
+            lista_produtos.Add("Mouse", 29.5);
+            lista_produtos.Add("Monitor", 780.85);
+            lista_produtos.Add("Memória 16Gb", 700.00);
+            lista_produtos.Add("Memória 8Gb", 375.65);
+            lista_produtos.Add("Processador", 1250);
+            lista_produtos.Add("Placa Mãe", 623.75);
+            lista_produtos.Add("Gabinete mATX", 150);
+            lista_produtos.Add("Gabinete ATX", 190);
+            #endregion
+
+            #region Lista Estados
+            lista_estados = new Dictionary<string, string>();
+            lista_estados.Add("Rio de Janeiro", "Brasil");
+            lista_estados.Add("New York", "Estados Unidos");
+            lista_estados.Add("São Paulo", "Brasil");
+            lista_estados.Add("Porto", "Portugal");
+            lista_estados.Add("Lisboa", "Portugal");
+            lista_estados.Add("Bahia", "Brasil");
+            lista_estados.Add("Whashington", "Estados Unidos");
+            lista_estados.Add("Algarve", "Portugal");
+            #endregion
+
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // Metodo Comum
+            //foreach (int num in lista_numeros)
+            //{
+            //    if (num % 2 == 0)
+            //    {
+            //        lista.Items.Add(num);
+            //    }
+            //}
+
+
+            // Utilizando LINQ
+            // Obter a fonte dos dados
+            // Criar a consulta.
+            // Exucutar a consulta
+
+            //IEnumerable<int> res = from num in lista_numeros where num % 2 == 0 select num;
+
+            //foreach (int n in res)
+            //{
+            //    lista.Items.Add(n);
+            //}
+
+            string txt = txtConsulta.Text;
+            IEnumerable<string> res2 = from nome in lista_nomes 
+                                       where nome.StartsWith(txt) 
+                                       select nome;
+
+            lista.Items.AddRange(res2.ToArray());
+
+            //foreach (string n in res2)
+            //{
+            //    lista.Items.Add(n);
+            //}
+
+
+        }
+
+        private void btnWhere_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            // Operador de Filtragem, a clausula where
+
+            string txt = txtConsulta.Text.ToLower();
+
+            var res = from nome in lista_nomes 
+                      where nome.ToLower().Contains(txt)
+                      select nome;
+
+
+            foreach (var n in res)
+            {
+                lista.Items.Add(n);
+            }
+
+        }
+
+        private void btnOrderBy_Click(object sender, EventArgs e)
+        {
+            // Operador de ordenação
+            lista.Items.Clear();
+            string txt = txtConsulta.Text;
+
+            //var res = from num in lista_numeros 
+            //          orderby num descending 
+            //          where num <= 10 
+            //          select num;
+
+            //var res = from nome in lista_nomes orderby nome  select nome;
+
+            //foreach (var n in res)
+            //{
+            //    lista.Items.Add(n);
+            //}
+
+            var res = from produto in lista_produtos
+                      orderby produto.Key
+                      select produto;
+
+            foreach (KeyValuePair<string,  double> item in res)
+            {
+                lista.Items.Add(item.Key + " R$ " + item.Value);
+            }
+
+            
+        }
+
+        private void btnGroupBy_Click(object sender, EventArgs e)
+        {
+            // Operadores de Agrupamento
+            lista.Items.Clear();
+            txtConsulta.Text = "";
+
+            var res = from estado in lista_estados 
+                      group estado by estado.Value;
+
+            foreach (var grupo in res)
+            {
+                lista.Items.Add(grupo.Key);
+                foreach (var estado in grupo)
+                {
+                    lista.Items.Add("     " + estado.Key);
+                }
+            }
+        }
+    }
+}
